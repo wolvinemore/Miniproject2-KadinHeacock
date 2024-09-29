3a1e57fc244a1dc1513b9eb2ffe4920918b25ba7
Wolvinemore <kadinmheacock7@gmail.com>
2024-09-29 17:53:29 -0500

    add basic starter info and variable that pulls data from the csv file.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+### INF601 - Advanced Programming in Python
+### Kadin Heacock
+### Miniproject1
+
 # imports
 import os
 import sys
@@ -7,7 +11,12 @@ import matplotlib.pyplot as plt
 import datetime
 import pprint
 
-ticker = (["panda", "panda"])
+# Reading the .csv file
+data = pd.read_csv( filepath_or_buffer='WLD_RTFP_country_2023-10-02.csv', index_col=0, parse_dates=True)
+
+plt.show()
 
 for i in ticker:
-    print(i)
+
+    df = pd.DataFrame({})
+
