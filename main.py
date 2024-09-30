eda8fb3e6ef80e9431f32c4269d786b9c627dd91
Wolvinemore <kadinmheacock7@gmail.com>
2024-09-29 22:56:37 -0500

    added the plotting capability as well as adding charts and printing columns.

--- main.py
+++ main.py
@@ -11,14 +11,36 @@ import matplotlib.pyplot as plt
 import datetime
 import pprint
 
+if not os.path.exists("Charts"):
+    os.mkdir("Charts")
+
 # Reading the .csv file
-data = pd.read_csv( filepath_or_buffer='WLD_RTFP_country_2023-10-02.csv', index_col=0, parse_dates=True)
+data = pd.read_csv( filepath_or_buffer= 'WLD_RTP_details_2023-10-02.csv', index_col=0, parse_dates=True)
 
-print()
+df = pd.DataFrame(data)
 
-plt.show()
+for col in df.columns:
+    series = df[col]
 
-for i in ticker:
+    print(series)
 
-    df = pd.DataFrame({})
 
+
+    # numpy to give the number of country's
+    a = np.array(countrys)
+
+    if len(countrys) == 26:
+        # Example: Plot the first numeric column against the index
+        plt.figure(figsize=(10, 6))
+        plt.plot(data.index, data.iloc[:, 0], label=data.columns[0])  # Plotting the first numeric column
+        plt.title('Time Series Plot of ' + data.columns[0])
+        plt.xlabel('Date')
+        plt.ylabel(data.columns[0])
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        row.plot(title=f'Row {index}', xlabel='Column Index', ylabel='Value')
+        plt.savefig(f'Charts\\{food_inflation}.png')
+        plt.show()
+    else:
+        print("No numeric data available for plotting.")
