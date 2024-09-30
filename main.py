2e19ae7a162709ec1fb831b786143460d5ed616d
Wolvinemore <kadinmheacock7@gmail.com>
2024-09-30 16:53:49 -0500

    updated the plotting capability

--- main.py
+++ main.py
@@ -8,39 +8,37 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime
-import pprint
 
+# Saves the graphs to the Charts file
 if not os.path.exists("Charts"):
     os.mkdir("Charts")
 
 # Reading the .csv file
 data = pd.read_csv( filepath_or_buffer= 'WLD_RTP_details_2023-10-02.csv', index_col=0, parse_dates=True)
 
+country = ["june"]
+
 df = pd.DataFrame(data)
 
-for col in df.columns:
-    series = df[col]
+def col():
+# Used to grab the 2 columns needed for the graph
+    for col in df.columns:
+        series = df[col]
 
-    print(series)
 
-
-
-    # numpy to give the number of country's
-    a = np.array(countrys)
-
-    if len(countrys) == 26:
-        # Example: Plot the first numeric column against the index
-        plt.figure(figsize=(10, 6))
-        plt.plot(data.index, data.iloc[:, 0], label=data.columns[0])  # Plotting the first numeric column
-        plt.title('Time Series Plot of ' + data.columns[0])
-        plt.xlabel('Date')
-        plt.ylabel(data.columns[0])
-        plt.legend()
-        plt.grid()
-        plt.tight_layout()
-        row.plot(title=f'Row {index}', xlabel='Column Index', ylabel='Value')
-        plt.savefig(f'Charts\\{food_inflation}.png')
-        plt.show()
-    else:
-        print("No numeric data available for plotting.")
+    # Graphically display the country's and there inflation percentage in 2023
+if len(country) == 1:
+    # Example: Plot the first numeric column against the index
+    plt.figure(figsize=(10, 6))
+    plt.plot(data.index, data.iloc[:, 0], label=data.columns[0])  # Plotting the first numeric column
+    plt.title('Time Series Plot of ' + data.columns[0])
+    plt.xlabel('Date')
+    plt.ylabel(data.columns[0])
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+  #  row.plot(title=f'Row {index}', xlabel='Column Index', ylabel='Value')
+    plt.savefig(f'Charts\\{country}.png')
+    plt.show()
+else:
+    print("No numeric data available for plotting.")
